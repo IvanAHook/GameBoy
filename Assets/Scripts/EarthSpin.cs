c9549411fbf6616cc5315a9c9bc42ac7f12de8c5
Ivan Höök <ivan.a.hook@gmail.com>
2016-05-24 13:35:03 +0200

    init commit

---
+++ Assets/Scripts/EarthSpin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpin : MonoBehaviour
+{
+	public float RotSpeed = 1;
+	
+	void Update () {
+		transform.Rotate(new Vector3(0,1,0)*RotSpeed, Space.Self);
+	}
+}
