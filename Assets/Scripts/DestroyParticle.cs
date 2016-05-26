7305e55f458aa16c0b78b21a6decbc569304f7d1
Ivan Höök <ivan.a.hook@gmail.com>
2016-05-26 09:58:18 +0200

    can now shoot + muzzle flash tweak

---
+++ Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour
+{
+
+	private ParticleSystem _particle;
+
+	void Awake()
+	{
+		_particle = GetComponent<ParticleSystem>();
+	}
+
+	void Update () {
+		if (!_particle.IsAlive())
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public ParticleSystem Particle;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var position = transform.position + Vector3.forward*-6;
+			position = new Vector3(position.x, position.y+1, position.z);
+			Instantiate(Particle, position, Quaternion.identity);
+		}
+
+	}
+}
