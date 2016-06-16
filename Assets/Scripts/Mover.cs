cdd0642ab1b3d3b0a17987690f4fee6672c432c8
Ivan Höök <ivan.a.hook@gmail.com>
2016-06-16 23:56:22 +0200

    lots of terminal work

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,6 +7,8 @@ public class Mover : MonoBehaviour
 	private NavMeshAgent _navMeshAgent;
 	private Camera _camera;
 
+	private bool _moving;
+
 	void Start ()
 	{
 		_camera = Camera.main;
@@ -24,7 +26,45 @@ public class Mover : MonoBehaviour
 				Transform objectHit = hit.transform;
 
 				_navMeshAgent.destination = hit.point;
+				_moving = true;
 			}
+		}
+		if (_moving)
+		{
+			if (HasReachedDestination())
+				Debug.Log ("YAAAAY");
+		}
+
+	}
+
+	private bool HasReachedDestination() {
+		// Check if we've reached the destination
+		if (!_navMeshAgent.pathPending) {
+			if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance) {
+				if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f) {
+					_moving = false;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.transform.tag == "Player")
+		{
+			Debug.Log (string.Format("{0} Enter terminal collider", other.transform.name));
+			// open terminal
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			Debug.Log (string.Format("{0} Exit terminal collider", other.transform.name));
+			// set Terminal visuals to idle.
 		}
 	}
 
--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -7,21 +8,53 @@ using UnityEngine.UI;
 
 public class Terminal : MonoBehaviour
 {
+
+	[Serializable]
+	public struct Command
+	{
+		public string name;
+		public UnityEvent action;
+	}
+
 	public Text TerminalText;
+	public InputField TerminalInputField;
 
 	private static readonly char[] DelimiterChars = { ' ' };
 	private Dictionary<string, Action<string[]>> _commands;
 
-	void Start () {
+	public Command[] Commands;
+
+	private void Start ()
+	{
 		// populate _commands
 		_commands = new Dictionary<string, Action<string[]>>();
-		_commands.Add("puss", strings =>
+		_commands.Add("PUSS", strings =>
 		{
 			foreach (var s in strings)
 			{
-				Debug.Log("... " + s);
+				PrintToTerminal("..." + s);
 			}
 		});
+		_commands.Add ("HELP", strings =>
+		{
+
+			PrintToTerminal("");
+			PrintToTerminal("LOCAL OPERATIONS:");
+			for(int i = 0; i < _commands.Count; i++)
+			{
+				PrintToTerminalTabbed(_commands.Keys.ElementAt(i));
+			}
+
+			PrintToTerminal("");
+			PrintToTerminal("EXTERNAL OPERATIONS:");
+			for(int i = 0; i < Commands.Length; i++)
+			{
+				PrintToTerminalTabbed(Commands[i].name);
+			}
+		});
+
+		TerminalInputField.caretWidth = 10;
+		TerminalInputField.caretBlinkRate = 1;
 	}
 
 	public void ParseCommand(string input)
@@ -31,39 +64,56 @@ public class Terminal : MonoBehaviour
 		commands.Remove(command);
 		var args = commands.ToArray();
 
-		input = Regex.Replace(input, @"\s+", "");
-
-		Debug.Log(input);
+		//input = Regex.Replace(input, @"\s+", "");
 
 		Action<string[]> action;
-		if (_commands.TryGetValue(command, out action))
+		if(_commands.TryGetValue(command, out action))
 		{
+			PrintToTerminal(string.Format("> {0}", input));
 			action.Invoke(args);
-			TerminalText.text += Environment.NewLine + string.Format("su@root: {0}", input);
-			TerminalText.text += Environment.NewLine + string.Format("executing {0}", command);
+			ResetTerminalInputField();
 			return;
-			//return string.Format("executing {0}", command);
 		}
-		TerminalText.text = string.Format("command {0} does not exist", command);
-		//return string.Format("command {0} does not exist", command);
+		else
+		{
+			for(int i = 0; i < Commands.Length; i++)
+			{
+				if(Commands[i].name == command)
+				{
+					PrintToTerminal(string.Format("> {0}", input));
+					PrintToTerminal(string.Format("EXECUTING {0}", command));
+					Commands[i].action.Invoke();
+					ResetTerminalInputField();
+					return;
+				}
+			}
+		}
+
+		PrintToTerminal(string.Format("COMMAND {0} NOT RECOGNIZED...", command));
+		ResetTerminalInputField();
+		TerminalText.text = TerminalText.text.ToUpper();
 	}
 
-	void OnTriggerEnter(Collider other) 
+	private void PrintToTerminal(string input)
 	{
-		if (other.transform.tag == "Player")
-		{
-			Debug.Log (string.Format("{0} Enter terminal collider", other.transform.name));
-			// open terminal
-		}
+		TerminalText.text += Environment.NewLine + string.Format(input);
 	}
 
-	void OnTriggerExit(Collider other)
+	private void PrintToTerminalTabbed(string input)
 	{
-		if (other.transform.tag == "Player")
-		{
-			Debug.Log (string.Format("{0} Exit terminal collider", other.transform.name));
-			// set Terminal visuals to idle.
-		}
+		TerminalText.text += Environment.NewLine + "    " + string.Format(input);
+	}
+
+	private void ResetTerminalInputField()
+	{
+		TerminalInputField.Select();
+		TerminalInputField.ActivateInputField();
+		TerminalInputField.text = "";
+	}
+
+	public void OnValueChanged()
+	{
+		TerminalInputField.text.ToUpper(); /*= string.Format("> {0}", TerminalInputField.text.ToUpper());*/
 	}
 
 }
