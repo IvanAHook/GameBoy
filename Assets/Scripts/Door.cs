d85c29edd5da3429f7921fe3b55ab095838079d6
Ivan Höök <ivan.a.hook@gmail.com>
2016-06-20 20:57:32 +0200

    ADDED DOOR

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Door : MonoBehaviour {
+
+	public Transform LeftDoor;
+	public Transform RightDoor;
+
+	private bool _isOpen = false;
+
+	public void Open(string args)
+	{
+		if(args == "OPEN")
+		{
+			LeftDoor.DOLocalMoveX(-1.5f, 1f);
+			RightDoor.DOLocalMoveX(1.5f, 1f);
+		} else if(args == "CLOSE")
+		{
+			LeftDoor.DOLocalMoveX(-0.5f, 1f);
+			RightDoor.DOLocalMoveX(0.5f, 1f);
+		}
+
+	}
+
+}
